bf672e96ada632faa199acc9946247de1835aaad
nagopi <nagopi@iu.edu>
2024-01-25 15:37:39 -0500

    added src, updated get method

--- src/index.js
+++ src/index.js
@@ -6,11 +6,8 @@ app.get('/', (req, res) => {
   res.send('Arithmetic service - Hello World!');
 });
 
-app.get('/sum', (req, res) => {
-  const num1 = Number(req.query.num1);
-  const num2 = Number(req.query.num2);
-  const sum = num1 + num2;
-  res.send(`${num1} + ${num2} = ${sum}`);
+app.get('/add/:n/:m', (req, res) => {
+  res.json(Number(req.params.n) + Number(req.params.m));
 });
 
 app.listen(port);
