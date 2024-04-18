75ef438ebdc24721de77d489c5e22efba28cfac4
nagopi <nagopi@iu.edu>
2024-04-18 16:01:17 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { add } = require('./arithmetica');
 const app = express();
-const port = 3000;
-
 app.use(cors());
 
+if (!process.env.PORT) {
+  throw new Error('PORT is not defined');
+}
+
+const port = process.env.PORT;
+
 app.get('/', (req, res) => {
-  res.send('Arithmetic service - Hello World!');
+  res.send('Arithmetic service - last update 3/5/2024');
 });
 
 app.get('/add/:n/:m', (req, res) => {
